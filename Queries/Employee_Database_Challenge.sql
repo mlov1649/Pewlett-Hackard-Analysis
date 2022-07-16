d85b05818cf93c97c84fe16c4c2ade3db5f80bc4
Lin Mitchell <mlovell92@gmail.com>
2022-07-16 14:50:01 -0400

    Update Employee_Database_Challenge.sql

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -55,14 +55,3 @@ ON em.emp_no = ti.emp_no
 WHERE (em.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY em.emp_no;
-
-SELECT * FROM mentorship_eligibility;-- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
-
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
